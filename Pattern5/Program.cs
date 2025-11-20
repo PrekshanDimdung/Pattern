f979951d6108bf28b66237a06f0fe1d944be295b
Prekshan Dimdung <prekshan@example.com>
2025-11-20 19:35:31 +0545

    Pattern5

---
+++ Pattern5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Pattern5
+{
+    static void Main()
+    {
+        int n = 8;
+        for (int i = 1; i <= n; i++)
+        {
+            for(int k = 1; k<=i; k++)
+                Console.Write("*");
+            
+            Console.Write(" ");
+            for(int k = 1; k<=i; k++)
+             Console.Write("*");
+            
+            Console.WriteLine(" ");
+        }
+    }
+}
