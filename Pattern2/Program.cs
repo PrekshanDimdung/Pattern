11e6801a7a46dd8cdcf5eaa746647f3d74e1a9b0
Prekshan Dimdung <prekshan@example.com>
2025-11-20 13:52:05 +0545

    Add Pattern2

---
+++ Pattern2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Pattern2
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 8; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
