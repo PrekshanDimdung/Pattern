20f577a3ad0e9f022e0e17c4a07f0e65c177c8b8
Prekshan Dimdung <prekshan@example.com>
2025-11-20 19:37:36 +0545

    Pattern4

---
+++ Pattern4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 7; //number of rows
+
+        for (int i = 1; i <= n; i++)
+        {
+            //spaces
+            for (int s = 1; s <= n - i; s++)
+            {
+                Console.Write(" ");
+            }
+
+            //stars
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
