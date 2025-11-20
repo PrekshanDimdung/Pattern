c09e161e597595d74ef357462499e28b458dae35
Prekshan Dimdung <prekshan@example.com>
2025-11-20 14:11:18 +0545

    Add Pattern3

--- Pattern3/Program.cs
+++ Pattern3/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 7; // height of upper part (middle longest line)
+
+        // Upper part
+        for (int i = 1; i <= n; i++)
+        {
+            for (int s = 1; s <= n - i; s++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--)
+        {
+            for (int s = 1; s <= n - i; s++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
